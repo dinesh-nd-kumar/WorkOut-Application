68f813c65e567194b2eb8ab31b13bacc05d6bfe7
dinesh-nd-kumar <115875336+dinesh-nd-kumar@users.noreply.github.com>
2024-08-03 14:28:05 +0530

    two timers complited wwith image changes

--- app/src/main/java/com/dineshdk/workoutapplication/ExerciseActivity.kt
+++ app/src/main/java/com/dineshdk/workoutapplication/ExerciseActivity.kt
@@ -3,6 +3,8 @@ package com.dineshdk.workoutapplication
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,6 +22,9 @@ class ExerciseActivity : AppCompatActivity() {
     private var exTimer: CountDownTimer? = null
     private var exProgress = 0
 
+    private var exerciseList : ArrayList<Exercise>? = null
+    private var currentExercisePosition = -1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,10 +38,19 @@ class ExerciseActivity : AppCompatActivity() {
         binding.toolbar.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        exerciseList = Constants.defaultExerciseList()
         setUpRestView()
 
     }
     private fun setUpRestView(){
+
+        binding.flRestView.visibility = VISIBLE
+        binding.tvTitle.visibility = VISIBLE
+        binding.tvExerciseName.visibility = INVISIBLE
+        binding.flExerciseView.visibility = INVISIBLE
+        binding.ivImage.visibility = INVISIBLE
+
         if (restTimer != null){
             restTimer?.cancel()
             restProgress = 0
@@ -46,21 +60,25 @@ class ExerciseActivity : AppCompatActivity() {
 
     private fun setUpExView(){
 
-        binding.flProgressbar.visibility = View.INVISIBLE
-        binding.tvTitle.text = "Exercise Name"
-        binding.flExerciseView.visibility = View.VISIBLE
+        binding.flRestView.visibility = INVISIBLE
+        binding.tvTitle.visibility = INVISIBLE
+        binding.tvExerciseName.visibility = VISIBLE
+        binding.flExerciseView.visibility = VISIBLE
+        binding.ivImage.visibility = VISIBLE
 
         if (exTimer != null){
             exTimer?.cancel()
             exProgress = 0
         }
+        binding.ivImage.setImageResource(exerciseList!![currentExercisePosition].getImage())
+        binding.tvExerciseName.text = exerciseList!![currentExercisePosition].getName()
         setExerciseProgressBar()
     }
 
     private fun setRestProgressBar(){
         binding.progressbar.progress = restProgress
 
-        restTimer = object : CountDownTimer(10000,1000){
+        restTimer = object : CountDownTimer(2000,1000){
             override fun onTick(p0: Long) {
                 restProgress++
                 binding.progressbar.progress = 10 - restProgress
@@ -69,6 +87,7 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
+                currentExercisePosition++
                 setUpExView()
 
             }
@@ -78,7 +97,7 @@ class ExerciseActivity : AppCompatActivity() {
     private fun setExerciseProgressBar(){
         binding.progressbarEx.progress = exProgress
 
-        exTimer = object : CountDownTimer(30000,1000){
+        exTimer = object : CountDownTimer(3000,1000){
             override fun onTick(p0: Long) {
                 exProgress++
                 binding.progressbarEx.progress = 30 - exProgress
@@ -87,7 +106,15 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity, "go to rest", Toast.LENGTH_SHORT).show()
+                if(currentExercisePosition < exerciseList!!.size -1){
+                    setUpRestView()
+                }else {
+                    Toast.makeText(
+                        this@ExerciseActivity,
+                        "Congrats! you complete exercise",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         }.start()
     }
